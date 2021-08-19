6dde829d91b03970122aa57a26877fa4610b80ad
andrea corronca <andrac1996@gmail.com>
2021-08-19 16:18:51 +0200

    created app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+//Router
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom" 
+
+//Components
+import Header from './components/header';
+import Home from './components/home';
+import Movie from './components/movie';
+import NotFound from './components/notFound';
+import Login from './components/login';
+
+//Context
+import UserProvider from './context';
+
+const App = () => (
+    <Router>
+      <UserProvider>
+        <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/:movieId" element={<Movie />} />
+            <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </UserProvider> 
+    </Router>
   );
-}
 
 export default App;
